4b9c5593a11c8a7f74da831ad9bfac71914a3d7a
JongseoLeeAI <JongseoLeeAI@gmail.com>
2019-09-10 17:13:04 +0900

    [190910] upload pandas_2 Matplotlib_1

---
+++ Matplotlib/myplot.py
@@ -0,0 +1,16 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.linspace(0, 10, 100)
+
+plt.plot(x, np.sin(x))
+plt.plot(x, np.cos(x))
+
+plt.show()
+
